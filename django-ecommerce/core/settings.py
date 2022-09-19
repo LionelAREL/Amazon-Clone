659f9b21204f9dfd5107797552b540ba07199343
Lionel AREL <lionel.arel@outlook.fr>
2022-09-19 23:09:49 +0200

    settings correct

--- django-ecommerce/core/settings.py
+++ django-ecommerce/core/settings.py
@@ -178,3 +178,38 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 SESSION_ENGINE = 'session.backends.db'
 
+
+
+LOGGING = {
+    'version': 1,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/var/log/django/django-error.log',
+            'formatter': 'simple'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    }
+}
+
+
