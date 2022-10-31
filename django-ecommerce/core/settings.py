e230f669054327653cdcc9de721e11efcc6c228f
Lionel AREL <lionel.arel@outlook.fr>
2022-10-31 10:59:24 +0100

    update s3

--- django-ecommerce/core/settings.py
+++ django-ecommerce/core/settings.py
@@ -222,3 +222,12 @@ LOGGING = {
         },
     }
 }
+
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_S3_ACCESS_KEY_ID = 'AKIAZZ3HSDVMGDEWC7MI'
+AWS_S3_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'ecommerce-storage-lionel-arel'
+AWS_S3_SIGNATURE_VERSION = "s3v4"
+AWS_S3_REGION_NAME = "eu-west-3"
