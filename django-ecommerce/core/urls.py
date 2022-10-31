2588797b9991df0045d739816757c635958dbda5
Lionel AREL <lionel.arel@outlook.fr>
2022-10-31 09:21:38 +0100

    update

--- django-ecommerce/core/urls.py
+++ django-ecommerce/core/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
 from django.contrib import admin
 
-
-path('backend/', include([
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authentification.urls')),
     path('api/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-]))
+]
