b6ca0e7c68dc7f725a33daac6f892e18d36645d6
Lionel AREL <lionel.arel@outlook.fr>
2022-11-15 02:28:45 +0100

    update

--- Scripts/afterInstall.sh
+++ Scripts/afterInstall.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#migrations
+source env/bin/activate
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py collectstatic --no-input
 # restart all services 
 systemctl restart nginx
 systemctl restart gunicorn
